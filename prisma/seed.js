c004aee4d5bca45465c2c808407e8c2f611f6d2f
DAscencioSanchez <diaspm77@gmail.com>
2022-05-09 14:07:28 -0500

    Agregando algunos registros para el modelo mission

--- prisma/seed.js
+++ prisma/seed.js
@@ -42,12 +42,62 @@ const prisma = new PrismaClient();
 				mission: 'Node'
       },
     });
-
     console.log('Create 3 explorers');
+
+    const mission1 = await prisma.mission.upsert({
+      where: { name: 'missionA' },
+      update: {},
+      create: {
+        name: 'woopi1',
+        lang: 'elixir',
+        mission: 'missionA',
+        missionCommander: 'Carlo',
+        enrollments: 5,
+        hasCertification: true
+      },
+
+    });
+
+    const mission2 = await prisma.mission.upsert({
+      where: { name: 'missionB' },
+      update: {},
+      create: {
+        name: 'woopi2',
+        lang: 'python',
+        mission: 'missionB',
+        missionCommander: 'Fernanda',
+        enrollments: 6,
+        hasCertification: false
+      },
+    });
+
+    const mission3 = await prisma.mission.upsert({
+      where: { name: 'missionC' },
+      update: {},
+      create: {
+        name: 'woopi3',
+        lang: 'CSS',
+        mission: 'missionC',
+        missionCommander: 'Rodrigo',
+        enrollments: 4,
+        hasCertification: true
+      },
+    });
+
+    
+    console.log('Create 3 missions');
+
   } catch(e) {
     console.error(e);
     process.exit(1);
   } finally {
     await prisma.$disconnect();
   }
+
+  
+})();
+
+
+(async function main() {
+ 
 })();
